cb942b9a2111c4c02212d62c3e0077146869fee4
Roberto Goz <63943375+Robertogoz@users.noreply.github.com>
2021-11-06 17:45:49 -0300

    Create ex01.c

---
+++ ex01.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int maior(int *a, int *b, int *c) {
+	if(*a > *b && *a > *c)
+		return *a;
+	else if(*b > *a && *b > *c)	
+		return *b;
+	else
+		return *c;
+}
+
+void ordenar(int *a, int *b, int *c) {
+	int h = maior(&a, &b, &c);
+	int i=0;
+	for(i=0;i<h;i++) {
+		if(i == *a) {
+			printf("%d ", *a);
+		} if(i == *b) {
+			printf("%d ", *b);
+		} if(i == *c) {
+			printf("%d ", *c);
+		}
+	}
+}
+
+int main(void) {
+	int a, b, c;
+	a=1;
+	b=3;
+	c=2;
+	ordenar(&a, &b, &c);
+}
+
+// expect to print: 1 2 3
+
