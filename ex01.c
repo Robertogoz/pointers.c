48023153d7ce8f7a4bf1cb8c4e004c7eb8e4ede7
Roberto Goz <63943375+Robertogoz@users.noreply.github.com>
2021-11-10 19:43:52 -0300

    Fixing ex01.c

--- ex01.c
+++ ex01.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int maior(int *a, int *b, int *c) {
-	if(*a > *b && *a > *c)
+int maior(int **a, int **b, int **c) {
+	if(**a > **b && **a > **c)
 		return *a;
-	else if(*b > *a && *b > *c)	
+	else if(**b > **a && **b > **c)	
 		return *b;
 	else
 		return *c;
@@ -32,4 +32,5 @@ int main(void) {
 }
 
 // expect to print: 1 2 3
+
 
